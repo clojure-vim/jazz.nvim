b4477051770869dfa1deb2aab27a95417c8f4fdb
Henry Kupty <hkupty@gmail.com>
2019-03-26 12:10:07 +0100

    WIP files

--- lua/jazz/files.lua
+++ lua/jazz/files.lua
@@ -7,8 +7,14 @@ local files = {}
 files.alternate = function(file)
   file = file or vim.api.nvim_call_function("expand", {"%:p"})
   local window = vim.api.nvim_get_current_win()
+  local winnr = vim.api.nvim_call_function("win_id2win", {window})
   local ns = vim.api.nvim_call_function("AcidGetNs", {file})
   local alternates = vim.api.nvim_call_function("AcidAlternateFiles", {file})
+
+  local new = function(ns, fname)
+    vim.api.nvim_call_function("AcidNewFile", {ns, fname})
+    vim.api.nvim_command(winnr .. "wincmd w | edit " .. fname)
+  end
 
   if utils.ends_with(ns, "-test") then
     ns = ns:sub(1, -5)
@@ -19,25 +25,32 @@ files.alternate = function(file)
   if #alternates == 0 then
     return
   elseif #alternates == 1 then
-      files.new(ns, alternates[1], window)
+      new(ns, alternates[1], window)
   else
     impromptu.filter{
       title = "🎵 Select which possible alternate file to create:",
       options = utils.map(function(itm) return {description = itm} end, alternates),
       handler = function(_, result)
-        files.new(ns, result.description, window)
+        new(ns, result.description, window)
         return true
       end
   }
   end
 end
 
-files.new = function(ns, fname, window)
-  ns = ns or vim.api.nvim_call_function("AcidGetNs", {})
+files.new = function(window)
   window = window or vim.api.nvim_get_current_win()
   local winnr = vim.api.nvim_call_function("win_id2win", {window})
-  local fpath = vim.api.nvim_call_function("AcidNewFile", {ns, fname})
-  vim.api.nvim_command(winnr .. "wincmd w | edit " .. fpath)
+    impromptu.form{
+      title = "🎵 New file's namespace:",
+      options = {ns = {description = "Namespace"}},
+      handler = function(_, result)
+        local fpath = vim.api.nvim_call_function("AcidNewFile", {result.ns, nil})
+        vim.api.nvim_command(winnr .. "wincmd w | edit " .. fpath)
+        return true
+      end
+  }
+
 end
 
 
